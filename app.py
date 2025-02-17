d6f5a3b58727900a4b769210687784a5ddc605e1
AnubhavLuciferMorningStar <97878104+AnubhavLuciferMorningStar@users.noreply.github.com>
2025-02-18 00:43:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -41,12 +41,11 @@ def input_image_setup(uploaded_file):
 st.set_page_config(page_title="Calories Advisor APP")
 
 st.header("Calories Advisor APP")
-input = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_container_width=True)
 
 submit = st.button("Tell me the total calories")
 
